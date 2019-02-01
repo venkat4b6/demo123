3241ac969c45b07b07b5be53814ac3191279c830
mahesh <mahesh@cloudjournee.com>
2019-02-01 07:19:30 +0530

    taskfile

---
+++ task1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+X=0.2
+Y=0.2
+echo $X
+echo $Y
+if  "$X -lt $Y" | bc  -l
+then
+     echo "X is less than Y"
+elif  "$X -gt $Y" | bc -l
+then
+     echo "X is greater than Y"
+elif  "$X -eq $Y" | bc -l
+then
+     echo "X is equal to Y"
+fi
